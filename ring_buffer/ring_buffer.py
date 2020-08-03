e0336967ac61c0f694ba11c607b05204b94b29d6
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2020-08-02 21:18:01 -0400

    Completed ring_buffer.py

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,17 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.count = 0
+        self.items = [None for i in range(capacity)]
 
     def append(self, item):
-        pass
+        if self.capacity == self.count:
+            self.items[0] = item
+            self.count = 1
+        else:
+            self.items[self.count] = item
+            self.count += 1
 
     def get(self):
-        pass
+        if self.count > 0:
+            return [i for i in self.items if i]
